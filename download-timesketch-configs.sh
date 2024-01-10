57c9c1ffa7c0ba2a932e975dab9a55802b64aeb7
janosch <jkpr@google.com>
2024-01-10 09:42:29 +0000

    Updated for the next version & workshop

--- download-timesketch-configs.sh
+++ download-timesketch-configs.sh
@@ -9,16 +9,17 @@ set -e
 # Create config directory
 mkdir -p configs/
 
-echo -n "* Fetching configuration files.."
+echo "* Fetching configuration files..."
 GITHUB_TS_BASE_URL="https://raw.githubusercontent.com/google/timesketch/master"
 GITHUB_TS_MOD_URL="https://raw.githubusercontent.com/jkppr/timesketch-configs/main"
 # Fetch default & modified Timesketch config files
 wget $GITHUB_TS_MOD_URL/data/timesketch.conf -O configs/timesketch.conf
-wget $GITHUB_TS_MOD_URL/data/tags.yaml -O configs/tags.yaml
+wget $GITHUB_TS_BASE_URL/data/tags.yaml -O configs/tags.yaml
 wget $GITHUB_TS_BASE_URL/data/plaso.mappings -O configs/plaso.mappings
 wget $GITHUB_TS_BASE_URL/data/plaso_formatters.yaml -O configs/plaso_formatters.yaml
 wget $GITHUB_TS_BASE_URL/data/generic.mappings -O configs/generic.mappings
-wget $GITHUB_TS_BASE_URL/data/features.yaml -O configs/features.yaml
+wget $GITHUB_TS_BASE_URL/data/winevt_features.yaml -O configs/winevt_features.yaml
+wget $GITHUB_TS_BASE_URL/data/regex_features.yaml -O configs/regex_features.yaml
 wget $GITHUB_TS_BASE_URL/data/ontology.yaml -O configs/ontology.yaml
 wget $GITHUB_TS_BASE_URL/data/intelligence_tag_metadata.yaml -O configs/intelligence_tag_metadata.yaml
 wget $GITHUB_TS_BASE_URL/data/sigma_config.yaml -O configs/sigma_config.yaml
@@ -27,4 +28,4 @@ wget $GITHUB_TS_MOD_URL/data/context_links.yaml -O configs/context_links.yaml
 wget $GITHUB_TS_BASE_URL/data/scenarios/facets.yaml -O configs/facets.yaml
 wget $GITHUB_TS_BASE_URL/data/scenarios/questions.yaml -O configs/questions.yaml
 wget $GITHUB_TS_BASE_URL/data/scenarios/scenarios.yaml -O configs/scenarios.yaml
-echo "OK"
+echo "DONE"
